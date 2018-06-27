c13f6d0ad675b95061faa0fde30c3dc2b3c8cc0c
James Stanley <james@incoherency.co.uk>
2018-06-27 20:49:48 +0100

    Implement 2-humans local play

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,32 +1,25 @@
 /* ui */
 $(document).ready(function() {
     var ws; // an IsopathWS from isopath-ws.js
+    var view; // an IsopathView from isopath-view.js
+
     var ingame = false;
     var ourturn = false;
     var connected = false;
-    var view;
+    var localgame = false;
 
     function ready() {
         $('#status').text('Ready.');
     }
 
-    var to_join;
-    function join_game(gameid) {
-        if (connected) {
-            join_game_now(gameid);
-        } else {
-            to_join = gameid;
-        }
-    }
-
-    function join_game_now(gameid) {
-        $('#lobby').hide();
-        $('#status').text("Waiting for websocket server...");
-        ws.joinGame(gameid);
-    }
-
     function redraw() {
         view.redraw();
+
+        if (localgame) {
+            $('#you-are').hide();
+        } else {
+            $('#you-are').show();
+        }
 
         if (ingame) {
             $('#are').text('are');
@@ -44,106 +37,155 @@ $(document).ready(function() {
         ingame = false;
     }
 
-    ws = new IsopathWS({
-        ws: "ws://" + window.location.hostname + ":" + window.location.port + "/ws",
-        awaitingOpponent: function(gameid) {
-            $('#opponent-link').attr('href', window.location + '#join-' + gameid);
-            $('#opponent-link').text(window.location + '#join-' + gameid);
-            $('#await-opponent').show();
-            ready();
-        },
-        gameEnded: function(reason) {
-            ingame = false;
-            redraw();
-            ready();
-        },
-        gameStarted: function(player) {
-            $('#yourcolour').text(player);
-            $('#await-opponent').hide();
-            $('#game').show();
-            view = new IsopathView({
-                isopath: ws.isopath,
-                redraw: function() {
-                    redraw();
-                },
-                can_click: function() {
-                    return ingame && ourturn;
-                },
-                clicked: function(p) {
-                    $('#illegal-move').text('');
-                },
-                move: function(m) {
-                    try {
-                        ws.playMove(m);
-                    } catch(e) {
-                        $('#illegal-move').text(e);
-                    };
-                    if (ws.isopath.winner())
-                        game_over();
-                },
-                move_history: function(html) {
-                    $('#movehistory').html(html);
-                },
-                partial_move: function(text) {
-                    $('#partial-move').text(text);
-                },
-            });
-            view.init_hexgrid('#hexgrid');
-            ingame = true;
-            ready();
-        },
-        movePlayed: function(player, move) {
-            if (ws.isopath.winner())
-                game_over();
-            redraw();
-            ready();
-        },
-        usToMove: function() {
-            $('#whoseturn').text('your');
-            ourturn = true;
-            $('#reset-move').show();
-            clickmode = '';
-            move = [];
-            ready();
-        },
-        opponentToMove: function() {
-            $('#whoseturn').text("your opponent's");
-            ourturn = false;
-            $('#reset-move').hide();
-            ready();
-        },
-        connected: function() {
-            connected = true;
-            if (to_join) {
-                join_game_now(to_join);
-            }
-            ready();
-        },
-        disconnected: function() {
-            $('#status').text("Disconnected.");
-            ingame = false;
-            connected = false;
-        },
-        error: function(err) {
-            $('#status').text("Error: " + err);
-        },
-    });
-    ws.connect();
-    $('#status').text("Connecting to websocket...");
+    function connect_websocket(connected_cb) {
+        ws = new IsopathWS({
+            ws: "ws://" + window.location.hostname + ":" + window.location.port + "/ws",
+            awaitingOpponent: function(gameid) {
+                $('#opponent-link').attr('href', window.location + '#join-' + gameid);
+                $('#opponent-link').text(window.location + '#join-' + gameid);
+                $('#await-opponent').show();
+                ready();
+            },
+            gameEnded: function(reason) {
+                ingame = false;
+                redraw();
+                ready();
+            },
+            gameStarted: function(player) {
+                $('#yourcolour').text(player);
+                $('#await-opponent').hide();
+                $('#game').show();
+                view = new IsopathView({
+                    isopath: ws.isopath,
+                    redraw: function() {
+                        redraw();
+                    },
+                    can_click: function() {
+                        return ingame && ourturn;
+                    },
+                    clicked: function(p) {
+                        $('#illegal-move').text('');
+                    },
+                    move: function(m) {
+                        try {
+                            ws.playMove(m);
+                        } catch(e) {
+                            $('#illegal-move').text(e);
+                        };
+                        if (ws.isopath.winner())
+                            game_over();
+                    },
+                    move_history: function(html) {
+                        $('#movehistory').html(html);
+                    },
+                    partial_move: function(text) {
+                        $('#partial-move').text(text);
+                    },
+                });
+                view.init_hexgrid('#hexgrid');
+                ingame = true;
+                ready();
+            },
+            movePlayed: function(player, move) {
+                if (ws.isopath.winner())
+                    game_over();
+                redraw();
+                ready();
+            },
+            usToMove: function() {
+                $('#whoseturn').text('your');
+                ourturn = true;
+                $('#reset-move').show();
+                clickmode = '';
+                move = [];
+                ready();
+            },
+            opponentToMove: function() {
+                $('#whoseturn').text("your opponent's");
+                ourturn = false;
+                $('#reset-move').hide();
+                ready();
+            },
+            connected: function() {
+                connected = true;
+                localgame = false;
+                connected_cb();
+                ready();
+            },
+            disconnected: function() {
+                $('#status').text("Disconnected.");
+                ingame = false;
+                connected = false;
+            },
+            error: function(err) {
+                $('#status').text("Error: " + err);
+            },
+        });
+        ws.connect();
+        $('#status').text("Connecting to websocket...");
 
-    // ping every 60s just to keep the websocket open
-    window.setInterval(function() {
-        ws.ping();
-    }, 60000);
+        // ping every 60s just to keep the websocket open
+        // TODO: if we ever end up creating and deleting multiple websockets, we need
+        // to keep track of & clear these timers
+        window.setInterval(function() {
+            ws.ping();
+        }, 60000);
+    }
+
+    $('#new-local-game').click(function() {
+        $('#lobby').hide();
+        $('#game').show();
+        localgame = true;
+
+        var isopath = new Isopath();
+
+        view = new IsopathView({
+            isopath: isopath,
+            redraw: function() {
+                redraw();
+            },
+            can_click: function() {
+                return ingame;
+            },
+            clicked: function(p) {
+                $('#illegal-move').text('');
+            },
+            move: function(m) {
+                try {
+                    isopath.playMove(m);
+                } catch(e) {
+                    $('#illegal-move').text(e);
+                };
+                $('#whoseturn').text(isopath.curplayer + "'s");
+                if (isopath.winner())
+                    game_over();
+            },
+            move_history: function(html) {
+                $('#movehistory').html(html);
+            },
+            partial_move: function(text) {
+                $('#partial-move').text(text);
+            },
+        });
+        view.init_hexgrid('#hexgrid');
+        ingame = true;
+        $('#whoseturn').text("white's");
+        redraw();
+    });
 
     $('#new-game').click(function() {
         $('#lobby').hide();
         $('#status').text("Waiting for websocket server...");
-        ws.newGame('white'); // TODO: we should be able to choose
+        connect_websocket(function(ws) {
+            ws.newGame('white'); // TODO: we should be able to choose
+        });
     });
 
     $('#join-game').click(function() {
-        join_game($('#gameid').val());
+        connect_websocket(function(ws) {
+            ws.joinGame($('#gameid').val());
+            $('#lobby').hide();
+        });
     });
 
     $('#await-opponent-cancel').click(function() {
@@ -160,9 +202,12 @@ $(document).ready(function() {
     $('#await-opponent').hide();
     $('#game').hide();
 
-    // join a game straight away if an id is specified in the fragment
+    // join a websocket game straight away if an id is specified in the fragment
     var match = window.location.hash.match(/#join-(.*)/);
     if (match) {
-        join_game(match[1]);
+        connect_websocket(function(ws) {
+            ws.joinGame(match[1]);
+            $('#lobby').hide();
+        });
     }
 });
