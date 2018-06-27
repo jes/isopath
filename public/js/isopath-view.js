a83a4f2a601cf57d2c572d8e217c85a047c490a4
James Stanley <james@incoherency.co.uk>
2018-06-27 14:44:05 +0100

    Refactor board view into isopath-view.js

---
+++ public/js/isopath-view.js
@@ -0,0 +1,120 @@
+/* code to draw an isopath game board and take player input */
+function IsopathView(opts) {
+    this.opts = opts;
+    this.idprefix = Date.now();
+    this.move = [];
+    this.clickmode = '';
+    this.movefrom = '';
+
+    this.place_to_hex = {
+                    a1:41, a2:42, a3:43, a4:44,
+                b1:34, b2:35, b3:36, b4:37, b5:38,
+            c1:27, c2:28, c3:29, c4:30, c5:31, c6:32,
+        d1:20, d2:21, d3:22, d4:23, d5:24, d6:25, d7:26,
+            e1:14, e2:15, e3:16, e4:17, e5:18, e6:19,
+                f1: 8, f2: 9, f3:10, f4:11, f5:12,
+                    g1: 2, g2: 3, g3: 4, g4: 5,
+    };
+}
+
+IsopathView.prototype.clicked_on_hex = function(place) {
+    var movedone = false;
+
+    this.opts.clicked(place);
+
+    if (!this.opts.can_click())
+        return;
+
+    var this_place_has = this.opts.isopath.piece_at(place);
+    if (this.move.length > 0 && this.move[0][0] == 'piece' && this.move[0][1] == place)
+        this_place_has = '';
+    if (this.move.length > 0 && this.move[0][0] == 'piece' && this.move[0][2] == place)
+        this_place_has = this.opts.isopath.curplayer;
+
+    if (this.clickmode == 'piece') {
+        if (this_place_has == '') {
+            this.move.push(["piece",this.movefrom,place]);
+            if (this.opts.isopath.homerow[this.opts.isopath.other[this.opts.isopath.curplayer]].indexOf(place) != -1) {
+                // we placed a piece on the enemy's homerow, no need for a second move-half
+                movedone = true;
+            }
+        }
+        this.clickmode = '';
+    } else if (this.clickmode == 'tile') {
+        if (this_place_has == '')
+            this.move.push(["tile",this.movefrom,place]);
+        this.clickmode = '';
+    } else {
+        this.clickmode = '';
+        if (this_place_has == this.opts.isopath.other[this.opts.isopath.curplayer]) {
+            // capture
+            this.move.push(["capture",place]);
+        } else if (this_place_has == this.opts.isopath.curplayer) {
+            // start moving a piece
+            this.clickmode = 'piece';
+            this.movefrom = place;
+        } else {
+            // start moving a tile
+            this.clickmode = 'tile';
+            this.movefrom = place;
+        }
+    }
+
+    if (movedone || this.move.length == 2) {
+        this.opts.move(this.move);
+        this.clickmode = '';
+        this.move = [];
+    }
+    this.opts.redraw();
+};
+
+IsopathView.prototype.redraw = function() {
+    for (var place in this.place_to_hex) {
+        var idx = this.place_to_hex[place];
+        var piece = this.opts.isopath.piece_at(place);
+        var height = this.opts.isopath.board[place];
+
+        // update state for completed halfmove
+        if (this.move.length == 1) {
+            if (this.move[0][0] == 'capture' && this.move[0][1] == place)
+                piece = '';
+            if (this.move[0][0] == 'tile' && this.move[0][1] == place)
+                height--;
+            if (this.move[0][0] == 'tile' && this.move[0][2] == place)
+                height++;
+            if (this.move[0][0] == 'piece' && this.move[0][1] == place)
+                piece = '';
+            if (this.move[0][0] == 'piece' && this.move[0][2] == place)
+                piece = this.opts.isopath.curplayer;
+        }
+
+        // update state for partial halfmove
+        if (this.clickmode == 'piece' && this.movefrom == place)
+            piece = '';
+        if (this.clickmode == 'tile' && this.movefrom == place)
+            height--;
+
+        $('#' + this.idprefix + '-hex-' + idx).css('background-image', 'url(/img/height' + height + piece + '.png');
+    }
+};
+
+// initialise the hex grid in the named div
+IsopathView.prototype.init_hexgrid = function(el) {
+    $(el).html('');
+    // 37 places, plus 8 invisible ones to get the spacing right
+    for (var i = 0; i < 45; i++) {
+        $('<div class="lab_item"><div class="hexagon hexagon2"><div class="hexagon-in1"><div class="hexagon-in2" id="' + this.idprefix + '-hex-' + (i+1) + '"></div></div></div></div>').appendTo(el);
+    }
+
+    var thisview = this;
+
+    for (var place in this.place_to_hex) {
+        var idx = this.place_to_hex[place];
+        let t = place;
+        $('#' + this.idprefix + '-hex-' + idx).click(function() {
+            thisview.clicked_on_hex(t);
+        });
+    }
+
+    this.redraw();
+};
--- public/js/ui.js
+++ public/js/ui.js
@@ -1,30 +1,16 @@
 /* ui */
 $(document).ready(function() {
-    // TODO: when we're black, invert the labelling vertically?
-    var place_to_hex = {
-                    a1:41, a2:42, a3:43, a4:44,
-                b1:34, b2:35, b3:36, b4:37, b5:38,
-            c1:27, c2:28, c3:29, c4:30, c5:31, c6:32,
-        d1:20, d2:21, d3:22, d4:23, d5:24, d6:25, d7:26,
-            e1:14, e2:15, e3:16, e4:17, e5:18, e6:19,
-                f1: 8, f2: 9, f3:10, f4:11, f5:12,
-                    g1: 2, g2: 3, g3: 4, g4: 5,
-    };
-
     var ws; // an IsopathWS from isopath-ws.js
     var ingame = false;
     var ourturn = false;
-    var ourcolour;
-    var opponentcolour;
-    var move = [];
-    var clickmode = '';
     var connected = false;
-    var to_join;
+    var view;
 
     function ready() {
         $('#status').text('Ready.');
     }
 
+    var to_join;
     function join_game(gameid) {
         if (connected) {
             join_game_now(gameid);
@@ -39,91 +25,8 @@ $(document).ready(function() {
         ws.joinGame(gameid);
     }
 
-    function clicked_on_hex(place) {
-        $('#illegal-move').text('');
-
-        var movedone = false;
-
-        if (ingame && ourturn) {
-            var this_place_has = ws.isopath.piece_at(place);
-            if (move.length > 0 && move[0][0] == 'piece' && move[0][1] == place)
-                this_place_has = '';
-            if (move.length > 0 && move[0][0] == 'piece' && move[0][2] == place)
-                this_place_has = ourcolour;
-
-            if (clickmode == 'piece') {
-                if (this_place_has == '') {
-                    move.push(["piece",movefrom,place]);
-                    if ((ourcolour == 'white' && ws.isopath.homerow["black"].indexOf(place) != -1)
-                            || (ourcolour == 'black' && ws.isopath.homerow["white"].indexOf(place) != -1)) {
-                        // we placed a piece on the enemy's homerow, no need for a second move-half
-                        movedone = true;
-                    }
-                }
-                clickmode = '';
-            } else if (clickmode == 'tile') {
-                if (this_place_has == '')
-                    move.push(["tile",movefrom,place]);
-                clickmode = '';
-            } else {
-                clickmode = '';
-                if (this_place_has == opponentcolour) {
-                    // capture
-                    move.push(["capture",place]);
-                } else if (this_place_has == ourcolour) {
-                    // start moving a piece
-                    clickmode = 'piece';
-                    movefrom = place;
-                } else {
-                    // start moving a tile
-                    clickmode = 'tile';
-                    movefrom = place;
-                }
-            }
-
-            if (movedone || move.length == 2) {
-                try {
-                    ws.playMove(move);
-                } catch(e) {
-                    $('#illegal-move').text(e);
-                };
-                if (ws.isopath.winner())
-                    game_over();
-                clickmode = '';
-                move = [];
-            }
-            redraw();
-        }
-    }
-
     function redraw() {
-        for (var place in place_to_hex) {
-            var idx = place_to_hex[place];
-            var piece = ws.isopath.piece_at(place);
-            var height = ws.isopath.board[place];
-
-            // update state for completed halfmove
-            if (move.length == 1) {
-                if (move[0][0] == 'capture' && move[0][1] == place)
-                    piece = '';
-                if (move[0][0] == 'tile' && move[0][1] == place)
-                    height--;
-                if (move[0][0] == 'tile' && move[0][2] == place)
-                    height++;
-                if (move[0][0] == 'piece' && move[0][1] == place)
-                    piece = '';
-                if (move[0][0] == 'piece' && move[0][2] == place)
-                    piece = ourcolour;
-            }
-
-            // update state for partial halfmove
-            if (clickmode == 'piece' && movefrom == place)
-                piece = '';
-            if (clickmode == 'tile' && movefrom == place)
-                height--;
-
-            $('#hex-' + idx).css('background-image', 'url(/img/height' + height + piece + '.png');
-        }
+        view.redraw();
 
         var moves = '';
         for (var i = 0; i < ws.isopath.moves.length; i++) {
@@ -153,11 +56,11 @@ $(document).ready(function() {
 
         if (ingame) {
             $('#are').text('are');
-            $('#whoseturn').show();
+            $('#whoseturn-div').show();
             $('#gameover').hide();
         } else {
             $('#are').text('were');
-            $('#whoseturn').hide();
+            $('#whoseturn-div').hide();
             $('#gameover').show();
         }
     }
@@ -190,24 +93,6 @@ $(document).ready(function() {
         return s;
     }
 
-    function init_hexgrid(player) {
-        $('#hexgrid').html('');
-        // 37 places, plus 8 invisible ones to get the spacing right
-        for (var i = 0; i < 45; i++) {
-            $('<div class="lab_item"><div class="hexagon hexagon2"><div class="hexagon-in1"><div class="hexagon-in2" id="hex-' + (i+1) + '"></div></div></div></div>').appendTo('#hexgrid');
-        }
-
-        for (var place in place_to_hex) {
-            var idx = place_to_hex[place];
-            let t = place;
-            $('#hex-' + idx).click(function() {
-                clicked_on_hex(t);
-            });
-        }
-
-        redraw();
-    }
-
     ws = new IsopathWS({
         ws: "ws://" + window.location.hostname + ":" + window.location.port + "/ws",
         awaitingOpponent: function(gameid) {
@@ -222,12 +107,31 @@ $(document).ready(function() {
             ready();
         },
         gameStarted: function(player) {
-            ourcolour = player;
-            opponentcolour = (player == 'white' ? 'black' : 'white');
             $('#yourcolour').text(player);
             $('#await-opponent').hide();
             $('#game').show();
-            init_hexgrid(player);
+            view = new IsopathView({
+                isopath: ws.isopath,
+                redraw: function() {
+                    redraw();
+                },
+                can_click: function() {
+                    return ingame && ourturn;
+                },
+                clicked: function(p) {
+                    $('#illegal-move').text('');
+                },
+                move: function(m) {
+                    try {
+                        ws.playMove(m);
+                    } catch(e) {
+                        $('#illegal-move').text(e);
+                    };
+                    if (ws.isopath.winner())
+                        game_over();
+                },
+            });
+            view.init_hexgrid('#hexgrid');
             ingame = true;
             ready();
         },
